a7f825caf0125625a3be63b4c6ea9880b1e0fa9b
sayali1205 <sayalikawarkar@gmail.com>
2023-07-10 16:11:27 +0530

    adding controller

---
+++ demoCrudGit/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,33 @@
+package com.example.demo.model;
+
+public class User {
+	private int uid;
+	private String uname;
+	private String uadd;
+	private long mobile;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getUadd() {
+		return uadd;
+	}
+	public void setUadd(String uadd) {
+		this.uadd = uadd;
+	}
+	public long getMobile() {
+		return mobile;
+	}
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+
+}
